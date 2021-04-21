c73cd76373aa50f0220792a9c4c475f3357f6317
samRgarcia <samuelrivas18@gmail.com>
2021-04-20 21:39:34 -0500

    feat: creation of funtion to control manage actions

---
+++ src/controllers/mechanic/getMechanic.js
@@ -0,0 +1,15 @@
+export async function getAllProblems( req, res) {
+    try {
+        res.status(200).json({data:"get get"})
+    }catch (e) {
+
+    }
+}
+
+export async function getGenerateIncomeByDate( req, res) {
+    try {
+        res.status(200).json({data:"get get"})
+    }catch (e) {
+
+    }
+}
---
+++ src/controllers/mechanic/postMechanic.js
@@ -0,0 +1,16 @@
+export async function postStartJob( req, res) {
+    try {
+        res.status(200).json({data:"get get"})
+    }catch (e) {
+
+    }
+}
+
+export async function postFinishJob( req, res) {
+    try {
+        res.status(200).json({data:"get get"})
+    }catch (e) {
+
+    }
+}
+
