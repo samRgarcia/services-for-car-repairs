e317497c80576b0b8214446aa4d364da393ecc24
samRgarcia <samuelrivas18@gmail.com>
2021-04-20 21:20:51 -0500

    feat: functions to approve and deny mechanical onlyations

--- src/controllers/client/postClient.js
+++ src/controllers/client/postClient.js
@@ -6,3 +6,19 @@ export async function postRegisterProblems( req, res) {
 
     }
 }
+
+export async function postApproveSolution( req, res) {
+    try {
+        res.status(200).json({data:"beta"})
+    }catch (e) {
+
+    }
+}
+
+export async function postDenySolution( req, res) {
+    try {
+        res.status(200).json({data:"beta"})
+    }catch (e) {
+
+    }
+}
